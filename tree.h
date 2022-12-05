a1c444ac668a6ea134458500888638fc42b9f6ff
williamdowden <90417984+williamdowden@users.noreply.github.com>
2022-12-04 23:22:28 -0500

    Update tree.h

--- tree.h
+++ tree.h
@@ -1,33 +1,36 @@
 // class declaration
 #pragma once
 #include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
 using namespace std;
 
-class BSTNode{ // nodes
-    private:
-        // variables
-        int count;
-        int key;
-        BSTNode *left;
-        BSTNode *right;
+class BSTNode { // nodes
+private:
+    // variables
+    int count;
+    int key;
+    BSTNode* left;
+    BSTNode* right;
 
-    public:
-        BSTNode(int key, int count); // constructor - makes new node, passes on the data
-        ~BSTNode(); // destructor - destroys old memory
-    
+public:
+    BSTNode(int key, int count); // constructor - makes new node, passes on the data
+    ~BSTNode(); // destructor - destroys old memory
+
     friend class BSTree; // can access private data from BSTree
 };
 
-class BSTree{ // binary search tree
-    private:
-        // variables
-        BSTNode *root;
+class BSTree { // binary search tree
+private:
+    // variables
+    BSTNode* root;
 
-    public:
-        BSTree(); // constructor
-        ~BSTree(); // destructor
+public:
+    BSTree(); // constructor
+    ~BSTree(); // destructor
 
-        // functions
-        void insert(int key);
-        int search(int key);
+    // functions
+    void insert(int key);
+    int search(BSTNode* root, int key);
 };
